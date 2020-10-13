25c00b6f34a59964b4d0dda2e5ac89743ad99b8f
Mario Vargas <mariovargas@Marios-MacBook-Pro.local>
2020-10-12 20:48:25 -0600

    Fixing unit test

--- pages/comingSoon/index.test.js
+++ pages/comingSoon/index.test.js
@@ -1,9 +1,10 @@
+import React from "react";
 import { render } from "@testing-library/react";
 import Index from "./index";
 
 describe("ComingSoon index test", () => {
   it("index page main text", async () => {
-    const { getByText } = render(<Index />);
-    getByText("COMING SOON!");
+    const { getByText } = render(<Index text="COMING SOON!" />);
+    expect(getByText("COMING SOON!"));
   });
 });
