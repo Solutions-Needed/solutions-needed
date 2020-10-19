045b94f64133a6b053698b7209ffbd3342827202
mariovaargas <52261455+mariovaargas@users.noreply.github.com>
2020-10-19 13:02:33 -0600

    Fixing ComingSoon task (#13)
    Co-authored-by: Mario Vargas <mariovargas@Marios-MacBook-Pro.local>

--- src/pages/comingSoon/index.js
+++ src/pages/comingSoon/index.js
@@ -1,10 +1,28 @@
-import { ComingSoon, Logo } from '@components/ComingSoon';
+import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
 
-const ComingSoonPage = () => (
-  <div className="py-20">
-    <Logo />
-    <ComingSoon />
-  </div>
-);
+const ComingSoonTitle = styled.h1`
+  font-size: 75px;
+  color: black;
+  text-align: center;
+  padding: 100px;
+`;
+
+const LogoImg = styled.img`
+  width: 200px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const ComingSoonPage = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="py-20">
+      <LogoImg src="/SolutionsNeededLogo.png" alt="Logo" />
+      <ComingSoonTitle>{t('COMING_SOON_TITLE')}</ComingSoonTitle>
+    </div>
+  );
+};
 
 export default ComingSoonPage;
--- src/tests/comingSoon/index.test.js
+++ src/tests/comingSoon/index.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { I18nextProvider } from 'react-i18next';
 import { render } from '@testing-library/react';
 import ComingSoonPage from '../../pages/comingSoon/index';
+import i18n from '../../i18n';
 
 describe('ComingSoon index test', () => {
   it('index page main text', async () => {
-    const { getByText } = render(<ComingSoonPage text="COMING SOON!" />);
-    expect(getByText('PRONTO!')).toBeDefined();
+    const { getByText } = render(
+      <I18nextProvider i18n={i18n}>
+        <ComingSoonPage />
+      </I18nextProvider>
+    );
+    expect(getByText('Pronto!')).toBeDefined();
   });
 });
