362717114ed87c491a22a46cf0000eeefc00f3f5
joalva18 <johan.murilloalvarado@ucr.ac.cr>
2020-10-19 23:48:58 -0600

    Adding Footer and Unit Test

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () =>{
+    return(
+      <footer>
+        <div className="py-20">
+            <h1 className="text-l text-center text-accent-1 p-8">
+            With ❤ for Solutions Needed Team 
+            </h1>
+        </div>
+      </footer>
+    )
+  }
+  
+export default Footer
